1b4d7d7177e0679a8fed9e2590bb1ec06b91b9cc
Lucas <sulac@live.fr>
2024-10-20 22:14:49 +0200

    five different material

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import { ThreeElement } from "./ThreeElement";
+import { useState } from "react";
 
 function App() {
+  const [isRotating, setIsRotating] = useState(false);
+  const toggleRotation = () => setIsRotating((prev) => !prev);
   return (
     <>
+      <button onClick={toggleRotation} id="rotateButton">
+        {isRotating ? "Stop Rotation" : "Start Rotation"}
+      </button>
       <Canvas
         id="main-canva"
-        camera={{ zoom: 1, position: [5, 5, 5], far: 15, near: 0.5 }}
+        camera={{ position: [5, 5, 5], far: 20, near: 0.5 }}
       >
         <color attach="background" args={["#fafafa"]} />
-        <OrbitControls />
+        <OrbitControls autoRotate={isRotating} />
         <axesHelper args={[6]} />
-        <gridHelper args={[10, 10, "#000000", "red"]} />
+        <gridHelper args={[10, 10]} />
         <ThreeElement />
       </Canvas>
     </>
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,84 +1,114 @@
-import { useFrame, useThree } from "@react-three/fiber";
-import { useRef } from "react";
-import { Group, MathUtils, Mesh } from "three";
+import { useTexture } from "@react-three/drei";
+import { useControls } from "leva";
+import { useEffect, useRef } from "react";
+import { Group, Mesh, NearestFilter } from "three";
 
 export const ThreeElement = () => {
-  const { size, gl, scene, camera } = useThree();
-  const purpleRef = useRef<Mesh>(null);
-  const tealRef = useRef<Mesh>(null);
-  const greenRef = useRef<Mesh>(null);
+  const meshRef = useRef<Mesh>(null);
   const groupRef = useRef<Group>(null);
-  const earthRef = useRef<Group>(null);
 
-  useFrame((state, delta, xrFrame) => {
-    if (
-      purpleRef.current &&
-      tealRef.current &&
-      greenRef.current &&
-      groupRef.current
-    ) {
-      purpleRef.current.rotation.x += 0.01;
-      tealRef.current.rotation.y += 0.01;
-      greenRef.current.rotation.z += 0.01;
-      groupRef.current.rotation.y += 0.01;
-    }
-    if (earthRef.current) {
-      earthRef.current.rotation.y += 0.01;
-    }
+  const controls = useControls({
+    thickness: { value: 0.1, min: 0.1, max: 10, step: 0.1 },
+    roughness: { value: 0.1, min: 0.1, max: 1, step: 0.1 },
+    metalness: { value: 0.1, min: 0.1, max: 1, step: 0.1 },
+    clearcoat: { value: 0.1, min: 0.1, max: 1, step: 0.1 },
+    clearcoatRoughness: { value: 0.1, min: 0.1, max: 1, step: 0.1 },
+    transmission: { value: 1, min: 0.1, max: 1, step: 0.1 },
+    ior: { value: 1.5, min: 0.1, max: 2.33, step: 0.1 },
   });
+
+  const matpCap = useTexture("./bloodMatCap.jpg");
+  const tone = useTexture("./threeTone.jpg");
+  tone.minFilter = NearestFilter;
+  tone.magFilter = NearestFilter;
+
+  useEffect(() => {
+    if (groupRef.current && meshRef.current) {
+      const meshLength = groupRef.current.children.length;
+
+      for (let i = 0; i < meshLength; i++) {
+        const mesh = groupRef.current.children[i] as Mesh;
+        mesh.geometry = meshRef.current.geometry;
+        mesh.position.x = (i % (meshLength / 2)) * 2 - 4;
+        mesh.position.z = 0;
+        if (i >= meshLength / 2) {
+          mesh.position.z = 2;
+        }
+      }
+    }
+    // if (meshRef.current && meshRefCopy1.current && meshRefCopy2.current) {
+    //    meshRefCopy1.current.geometry = meshRef.current.geometry;
+    //    meshRefCopy2.current.geometry = meshRef.current.geometry;
+    // }
+  }, []);
   return (
     <>
-      <group
-        position={[0, 0, 0]}
-        rotation={[
-          MathUtils.degToRad(0),
-          MathUtils.degToRad(45),
-          MathUtils.degToRad(0),
-        ]}
-        ref={groupRef}
-      >
-        <mesh
-          ref={purpleRef}
-          position={[0, 0, 0]}
-          scale={[1, 1, 1]}
-          rotation={[
-            MathUtils.degToRad(0),
-            MathUtils.degToRad(0),
-            MathUtils.degToRad(0),
-          ]}
-        >
-          <directionalLight position={[1, 0, 0]} intensity={3} />
-          <sphereGeometry />
-          <meshLambertMaterial color="gold" />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      {/* <fog attach="fog" args={["blue", 0, 15]} /> */}
+      <mesh ref={meshRef} position={[0, 0, 0]}>
+        <torusKnotGeometry args={[0.5, 0.1]} />
+        <meshBasicMaterial color="green" visible={false} />
+      </mesh>
+      <group ref={groupRef}>
+        <mesh>
+          <meshMatcapMaterial matcap={matpCap} />
         </mesh>
-        <group position={[4, 0, 0]} ref={earthRef}>
-          <mesh
-            ref={tealRef}
-            position={[2, 1, 0]}
-            scale={[0.5, 0.5, 0.5]}
-            rotation={[
-              MathUtils.degToRad(0),
-              MathUtils.degToRad(0),
-              MathUtils.degToRad(0),
-            ]}
-          >
-            <sphereGeometry />
-            <meshLambertMaterial color="#fafafa" />
-          </mesh>
-          <mesh
-            ref={greenRef}
-            position={[0, 0, 0]}
-            scale={[1, 1, 1]}
-            rotation={[
-              MathUtils.degToRad(0),
-              MathUtils.degToRad(0),
-              MathUtils.degToRad(0),
-            ]}
-          >
-            <sphereGeometry />
-            <meshLambertMaterial color="teal" />
-          </mesh>
-        </group>
+        <mesh>
+          <meshToonMaterial gradientMap={tone} color={"pink"} />
+        </mesh>
+        <mesh>
+          <meshDepthMaterial />
+        </mesh>
+        <mesh>
+          <meshStandardMaterial
+            color="red"
+            emissive="black"
+            roughness={1}
+            metalness={0}
+            fog
+          />
+        </mesh>
+        <mesh>
+          <meshPhysicalMaterial
+            fog
+            color="#fff"
+            emissive="black"
+            visible
+            opacity={1}
+            transparent
+            alphaTest={1}
+            depthTest
+            depthWrite
+            roughness={controls.roughness}
+            metalness={controls.metalness}
+            clearcoat={controls.clearcoat}
+            clearcoatRoughness={controls.clearcoatRoughness}
+            transmission={controls.transmission}
+            thickness={controls.thickness}
+            ior={controls.ior}
+          />
+        </mesh>
+        <mesh>
+          <meshBasicMaterial color="green" wireframe />
+        </mesh>
+        <mesh>
+          <meshBasicMaterial color="red" fog />
+        </mesh>
+        <mesh>
+          <meshLambertMaterial color="yellow" emissive="red" />
+        </mesh>
+        <mesh>
+          <meshPhongMaterial
+            color="blue"
+            emissive="black"
+            fog
+            specular="white"
+            shininess={40}
+          />
+        </mesh>
+        <mesh>
+          <meshNormalMaterial />
+        </mesh>
       </group>
     </>
   );
